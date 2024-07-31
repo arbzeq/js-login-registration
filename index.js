85fbc7e298a3e379705e1116d5606988071a6028
Arbnor Zeqiri <arbnor.zeqiri@yahoo.com>
2024-07-31 19:42:48 +0200

    Latest

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
-function validateRegistration(){
-    let regForm = document.querySelector("#regForm");
-    regForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-        console.log(event);
+let loginForm = document.querySelector("#loginForm");
+let regForm = document.querySelector("#regForm");
+let loginTab = document.querySelector("#loginTab");
+let regTab = document.querySelector("#regTab")
+let currentForm = loginForm;
 
-        try {
-            let name = event.target.name.value;
-            let username = event.target.username.value;
-            let email = event.target.name.value;
-            let password = event.target.password.value;
-            let confirmPassword = event.target.confirmPassword.value;
 
-            if(password != confirmPassword) {
-                throw new Error(`The passwords do not match! ${event.timeStamp}`);
-                
-            }
-            
-        } catch(error) {
-            errorElement = document.querySelector('.errorInfo');
-            errorElement.textContent = error.message;
-            errorElement.style.display = "block";
-        }
+loginTab.addEventListener("click", () => {
+    loginForm.classList.remove("dont-display");
+    regForm.classList.add("dont-display");
+    currentForm = loginForm;
+});
+
+    
+
+regTab.addEventListener("click", () => {
+    loginForm.classList.add("dont-display");
+    regForm.classList.remove("dont-display");
+    currentForm = regForm;
+});
+
+regForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    let eventTarget = event.target;
+    console.log(event);
+    try {
+        let name = eventTarget.name.value;
+        let username = eventTarget.username.value;
+        let email = eventTarget.name.value;
+        let password = eventTarget.password.value;
+        let confirmPassword = eventTarget.confirmPassword.value;
         
-    })
-}
+        if(password.length < 8) {
+            throw new Error(`The password is too short!`);
+        }
+        if(password != confirmPassword) {
+            throw new Error(`The passwords do not match! ${event.timeStamp}`);
+        }
 
 
-function setup(){
-    validateRegistration();
-}
-
-setup();
+        
+    } catch(error) {
+        
+        eventTarget.errorInfo.textContent = error.message;
+        eventTarget.style.display = "block";
+    }
+    
+})
